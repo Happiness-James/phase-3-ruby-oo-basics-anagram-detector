5f1ca7882530f33ea81aa15c01e484f00d4b9b9d
Happiness-James <gesarehjames@gmail.com>
2023-02-12 19:27:12 +0000

    assignment done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end 
+    def match(words)
+        words.filter { |letter|   letter.split("").sort == word.split("").sort }
+
+    end       
+end    
